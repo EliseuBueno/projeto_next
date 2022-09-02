0cfdee99d0bafb5282307287273b122817566753
Eliseu Bueno <sturarobuenoeliseu@gmail.com>
2022-09-02 18:16:41 -0300

    Feat: Criação de primeiro padrão para estilização de cabeçalho e rodapé

--- pages/products/pants/redpant.js
+++ pages/products/pants/redpant.js
@@ -1,3 +1,15 @@
+import { Container } from "@mui/material";
+import { AppBar } from "@mui/material";
+import { grey } from "@mui/material/colors";
+
 export default function RedPant() {
-    return <h1>Calça Vermelha - R$89,90</h1>
+    return (
+        <>
+            <AppBar position="static" sx={{ color: grey[900] }}>
+                <Container>
+                </Container>
+            </AppBar>
+            <h1>Calça Vermelha - R$89,90</h1>
+        </>
+    )
 }
--- pages/products/shirt.js
+++ pages/products/shirt.js
@@ -1,3 +1,9 @@
+import { Container } from "@mui/material";
+
 export default function Shirt() {
-    return <h1>Camisa R$19,90</h1>
+    return (
+        <Container>
+            <h1>Camisa R$19,90</h1>
+        </Container>
+    )
 }
